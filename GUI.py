d22982a0211fe03be34162b575d03d7a6a757c2c
emcristrel <94045340+emcristrel@users.noreply.github.com>
2022-12-25 11:51:30 +0100

    création threading

--- GUI.py
+++ GUI.py
@@ -3,6 +3,47 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox, QMessageBox, QTextEdit
 import threading
 
+
+class Client(threading.Thread):
+
+    def __init__(self, host, port):
+        super().__init__()
+        self.__host = host
+        self.__port = port
+        self.__socket_client = socket.socket()
+        print("Connexion établie...")
+
+    #méthode de connection
+    def __connect(self) -> int:
+        try :
+            self.__socket_client.connect((self.__host,self.__port))
+        except ConnectionRefusedError:
+            print ("serveur non lancé ou mauvaise information")
+            return -1
+        except ConnectionError:
+            print ("erreur de connection")
+            return -1
+        else :
+            print ("connexion réalisée")
+            return 0
+
+    # méthode de dialogue synchrone
+    def __dialogue(self):
+        msg = ""
+        while msg != "kill" and msg != "disconnect" and msg != "reset":
+            msg = input("client: ")
+            self.__socket_client.send(msg.encode())
+            msg = self.__socket_client.recv(1024).decode()
+            print(msg)
+        self.__socket_client.close()
+
+
+    def run(self):
+        if (self.__connect() ==0):
+            self.__dialogue()
+
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -12,7 +53,7 @@ class MainWindow(QMainWindow):
         widget.setLayout(grid)
 
 
-        self.lab = QLabel("Saisir votre message")
+        self.lab = QLabel("Saisissez votre commande")
         self.text = QLineEdit("")
         self.info = QTextEdit("")
         self.choix = QComboBox()
@@ -26,8 +67,11 @@ class MainWindow(QMainWindow):
         self.quit = QPushButton("Quitter")
         self.kill = QPushButton("Kill")
         self.reset = QPushButton("Reset")
-        self.aide =QPushButton("?")
-
+        self.aide = QPushButton("?")
+        self.ipserv = QLabel("IP serveur")
+        self.ip = QLineEdit("")
+        self.port = QLineEdit("")
+        self.connection = QPushButton("Connection")
 
         grid.addWidget(self.lab, 0, 0, 1, 2)
         grid.addWidget(self.text, 1, 0, 1, 2)
@@ -38,7 +82,11 @@ class MainWindow(QMainWindow):
         grid.addWidget(self.quit, 4, 0, 1, 4)
         grid.addWidget(self.reset, 5, 0, 1, 4)
         grid.addWidget(self.kill, 6, 0, 1, 4)
-        grid.addWidget(self.aide,7, 3)
+        grid.addWidget(self.aide,0, 3)
+        grid.addWidget(self.ipserv,7, 0)
+        grid.addWidget(self.ip,7, 1)
+        grid.addWidget(self.port,7, 2)
+        grid.addWidget(self.connection, 7, 3)
 
 
         self.okcbox.clicked.connect(self.__actionOkCbox)
@@ -47,9 +95,20 @@ class MainWindow(QMainWindow):
         self.kill.clicked.connect(self.__actionKill)
         self.reset.clicked.connect(self.__actionReset)
         self.aide.clicked.connect(self.__actionAide)
+        self.connection.clicked.connect(self.__actionConnect)
         self.setWindowTitle("Mon Application")
 
-        
+    
+    def __actionConnect(self):
+        addr=self.line_edit3.text()
+        port = int(addr.split(":")[1])
+        ip = addr.split(":")[0]
+        socket_client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_client.connect(ip, port)
+        print("Connexion établie ...")
+        QMessageBox.information(self, "Conexion", "Connexion réussie")
+
+
     def __actionOkText(self):
         mess=self.text.text()
         socket_client.send(mess.encode())
@@ -62,15 +121,7 @@ class MainWindow(QMainWindow):
         socket_client.send(self.choix.currentText().encode())
         data = socket_client.recv(1024).decode()
         self.info.append(f"{data}")
-    def connect(self):
-        addr=self.line_edit3.text()
-        port = int(addr.split(":")[1])
-        ip = addr.split(":")[0]
-        self.client_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(ip, port)
-        print("Connexion établie ...")
-        QMessageBox.information(self, "Conexion", "Connexion réussie")
-        self.message = "rien"
+
         
         """""
         try:
@@ -128,18 +179,41 @@ class MainWindow(QMainWindow):
         QMessageBox.information(self, "Aide","Choisissez une information du pc, de la vm que vous voulez récupérer (l'os, le nom, l'ip, ...).")
 
 
+    def CSV(self):
+
+        try:
+            with open("serv.csv") as file:
+                col_headers = ['IP', 'PORT']
+                self.table.setHorizontalHeaderLabels(col_headers)
+                reader = csv.reader(file, delimiter=":")
+                for i, row in enumerate(reader):
+                    for j, col in enumerate(row):
+                        self.table.setItem(i, j, QTableWidgetItem(col))
+        except:
+            with open("serv.csv", 'w') as file:
+                file.write('')
+
+
+        self.table.show()
+        self.show()
 
 
 if __name__ == '__main__':
-    menu = input("Pour se connecter au serveur n°1 taper 1. Pour se connecter au serveur n°2 taper 2 : ")
-    if menu == "1":
-        port = 10000
-    else:
-        port = 10001
 
-    socket_client = socket.socket()
-    print(f"localhost {port}")
-    socket_client.connect(("localhost", port))
+    if len(sys.argv) < 3:
+        client = Client("127.0.0.1",15001)
+    else :
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+        # création de l'objet client qui est aussi un thread
+        client = Client(host,port)
+    #démarrage de la thread client
+    client.start()
+    client.join()
+
+
+    #print(f"localhost {port}")
+    #socket_client.connect(("localhost"))
     print("Connexion établie...")
     app = QApplication(sys.argv)
     window = MainWindow()
--- serv1.py
+++ serv1.py
@@ -15,51 +15,51 @@ while msg!= "kill" and data!= "kill":
     serveur_socket.listen(1)
     print("Serveur démarré...")
 
-    while msg!="kill" and msg!="reset" and data!="kill" and data!="reset":
-        serv, addr = serveur_socket.accept()
+    while msg!="kill" and msg!="reset":
+        connect, addr = serveur_socket.accept()
 
-        while msg!= "kill" and msg!="reset" and msg!="disconnect" and data!= "kill" and data!="reset" and data!="disconnect":
-            data = serv.recv(1024).decode()
-            print(data)
+        while msg!= "kill" and msg!="reset" and msg!="disconnect":
+            data = connect.recv(1024).decode()
+            print ("Reçu du client: ", data)
 
             if data == "os" or data == "OS" :
                 os= platform.system()
                 msg=str(f"Operating system: {os}")
-                serv.send(msg.encode())
+                connect.send(msg.encode())
 
             elif data == "cpu" or data == "CPU" :
                 cpu =  psutil.cpu_percent(4)
                 msg=str(f"CPU: {cpu}")
-                serv.send(msg.encode())
+                connect.send(msg.encode())
             
             elif data == "ip" or data == "IP" :
                 netifaces.interfaces()
                 adresse_ip = netifaces.ifaddresses('en0')[2][0]['addr']
                 netaddr_adresse_ip = netaddr.IPAddress(adresse_ip)
                 msg=str(f"IP: {netaddr_adresse_ip}")
-                serv.send(msg.encode())
+                connect.send(msg.encode())
 
             elif data == "ram" or data == "RAM" :
                 ram=psutil.virtual_memory()[2]
                 msg=str(f'RAM memory % used:{ram}')
-                serv.send(msg.encode())
+                connect.send(msg.encode())
 
             elif data == "name" or data == "Name" :
                 name=platform.node()
                 msg=str(f"Mon nom est: {name}")
-                serv.send(msg.encode())
+                connect.send(msg.encode())
 
             elif data == "disconnect":
                 rien = ""
-                serv.send(rien.encode())
+                connect.send(rien.encode())
             
             elif data == "kill":
                 rien = ""
-                serv.send(rien.encode())
+                connect.send(rien.encode())
 
             elif data == "kill":
                 rien = ""
-                serv.send(rien.encode())
+                connect.send(rien.encode())
 
 
             else: 
@@ -70,17 +70,19 @@ while msg!= "kill" and data!= "kill":
                 if verif == 0:
 
                     if msg != "":
-                        serv.send(msg.encode())    
+                        connect.send(msg.encode())    
                     else:
-                        serv.send(f"{cmd} ok".encode())
+                        connect.send(f"{cmd} ok".encode())
                 else:
                     msg=str(f'{cmd} ne peut pas être lancée')
-                    serv.send(msg.encode())
+                    connect.send(msg.encode())
             
 
         
-        serv.close()
-        print ("CONNECTION FERMÉ !")
+        connect.close()
+        print ("Connection fermé")
+    serveur_socket.close()
+    print("Serveur fermé")
 
 
 if data == "kill" or msg == "kill":
