5131d882dafc520a31c98894764a19954fb12fee
emcristrel <94045340+emcristel@users.noreply.github.com>
2023-01-01 18:37:07 +0100

    ajout threading

--- GUI.py
+++ GUI.py
@@ -1,101 +1,8 @@
 import socket
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox, QMessageBox, QTextEdit
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox, QMessageBox, QTextEdit, QTextBrowser
+from PyQt6.QtCore import QThread
 import threading
-
-
-####                         ####
-#### PARTIE THREADING CLIENT ####
-####                         ####
-
-
-class Client(threading.Thread):
-
-
-    def __init__(self, host, port):
-        super().__init__()
-        self.__host = host
-        self.__port = port
-        self.__socket_client = socket.socket()
-        self.__thread = None
-        
-        print("Connexion établie...")
-
-    #méthode de connection
-    def __connect(self) -> int:
-        try :
-            self.__socket_client.connect((self.__host,self.__port))
-        except ConnectionRefusedError:
-            print ("serveur non lancé ou mauvaise information")
-            return -1
-        except ConnectionError:
-            print ("erreur de connection")
-            return -1
-        else :
-            print ("connexion réalisée")
-            return 0
-
-
-    def connection(self):
-        self.__socket_client.connect((self.__host, self.__port))
-
-    # méthode de dialogue synchrone
-    def __dialogue(self):
-        mess = ""
-        self.__thread = threading.Thread(target=self.__recv, args=[self.__socket_client, ])
-        self.__thread.start()
-        while mess != "kill" and mess != "disconnect" and mess != "reset":
-            mess = self.__send()
-        self.__thread.join()
-        self.__socket_client.close()
-    
-
-    def send(self, mess):
-        try:
-            self.__socket_client.send(mess.encode())
-            data = self.__socket_client.recv(1024).decode()
-        except:
-            print("Erreur d'envoi")
-        else:
-            return data
-
-
-    def __send(self):
-        mess = input("client:")
-        try:
-            self.__socket.send(mess.encode())
-        except BrokenPipeError:
-            print("Erreur")
-
-
-    def recv(self, socket_client):
-        mess=""
-        while mess != "kill" and mess != "disconnect" and mess != "reset":
-            mess = socket_client.recv(1024).decode()
-            input("Serveur:" )
-            print(mess)
-
-
-    def run(self):
-        if (self.__connect() ==0):
-            self.__dialogue()
-
-
-### MAIN ###
-
-
-if __name__=="__main__":
-    if len(sys.argv) < 3:
-        client = Client("127.0.0.1",15001)
-    else :
-        host = sys.argv[1]
-        port = int(sys.argv[2])
-        # création de l'objet client qui est aussi un thread
-        client = Client(host,port)
-    #démarrage de la thread client
-    client.start()
-    client.join()
-    print("Connexion établie...")
 
 
 
@@ -118,7 +25,8 @@ class MainWindow(QMainWindow):
         self.statut = QLabel("Déconnecté")
         self.lab = QLabel("Saisissez votre commande")
         self.text = QLineEdit("")
-        self.info = QTextEdit("")
+        self.info = QTextBrowser()
+        self.info.setReadOnly(True)
         self.choix = QComboBox()
         self.choix.addItem("OS")
         self.choix.addItem("RAM")
@@ -136,6 +44,8 @@ class MainWindow(QMainWindow):
         self.port = QLineEdit("10000")
         self.connection = QPushButton("Connection")
 
+        
+
         grid.addWidget(self.statut, 0, 0)
         self.statut.setStyleSheet("background-color: red; font-weight: bold; color: black;")
         grid.addWidget(self.lab, 1, 0, 1, 2)
@@ -152,11 +62,12 @@ class MainWindow(QMainWindow):
         grid.addWidget(self.ip, 8, 1)
         grid.addWidget(self.port, 8, 2)
         grid.addWidget(self.connection, 8, 3)
+    
 
 
         self.okcbox.clicked.connect(self.__actionOkCbox)
         self.ok.clicked.connect(self.__actionOkText)
-        self.quit.clicked.connect(self.__actionQuitter)
+        self.quit.clicked.connect(self.__actionDisconnect)
         self.kill.clicked.connect(self.__actionKill)
         self.reset.clicked.connect(self.__actionReset)
         self.aide.clicked.connect(self.__actionAide)
@@ -173,61 +84,116 @@ class MainWindow(QMainWindow):
 
         if self.statut.text() == 'Déconnecté':
             try:
-                self.__socket_client = Client(host, port)
-                self.__socket_client.connection()
+                self.__socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.__socket_client.connect((host, port))
+                self.thread_reception = ThreadReception(self.__socket_client, self.info)
+                self.thread_reception.start()
                 self.statut.setText("Connecté")
                 self.statut.setStyleSheet("background-color: blue; font-weight: bold; color: black;")
-
-            except ConnectionRefusedError:
-                QMessageBox.warning(self, "Conexion", "Erreur du serveur")
-                return -1
-            else:
                 QMessageBox.information(self, "Connexion", "Connexion réussie")
-                return 0
-        else:
-            QMessageBox.warning(self, "Conexion", "Connection déjà en cours")
+            except:
+                QMessageBox.warning(self, "Connexion", "Connexion échouée")
             
 
-    def __actionOkText(self):
+    def __actionDisconnect(self):
+            if self.statut.text() == 'Connecté':
+                self.__socket_client.send("close".encode())
+                self.__socket_client.close()
+                self.thread_reception.terminate()
+                self.info.clear()
+                self.statut.setText("Déconnecté")
+                self.statut.setStyleSheet("background-color: red; font-weight: bold; color: black;")
+                QMessageBox.information(self, "Deconnexion", "Deconnexion réussie")
+            else:
+                QMessageBox.warning(self, "Deconnexion", "Déja déconnecté")
 
-        self.__socket_client = Client(mess)
+
+    def __actionOkText(self):
         mess = self.text.text()
         self.__socket_client.send(mess.encode())
-        data = self.__socket_client.recv(1024).decode()
-        self.info.append(f"{data}")
-
+        self.text.clear()
 
     def __actionOkCbox(self):
         mess = self.choix.currentText()
         self.__socket_client.send(mess.encode())
-        data = self.__socket_client.recv(1024).decode()
-        self.info.append(f"{data}")
-               
-
-    def __actionQuitter(self):
-        mess= "disconnect"
-        self.__socket_client.send(mess.encode())
 
     
     def __actionKill(self):
-        mess= "kill"
-        self.__socket_client.send(mess.encode())
+        if self.statut.text() == 'Connecté':
+            self.__socket_client.send("close".encode())
+            self.__socket_client.close()
+            self.thread_reception.terminate()
+            self.info.clear()
+            self.statut.setText("Déconnecté")
+            self.statut.setStyleSheet("background-color: red; font-weight: bold; color: black;")
+            QMessageBox.information(self, "Deconnexion", "Deconnexion réussie")
+        else:
+            QMessageBox.warning(self, "Deconnexion", "Déja déconnecté")
         
 
     def __actionReset(self):
-        mess= "reset"
-        self.__socket_client.send(mess.encode())
+        if self.statut.text() == 'Connecté':
+            self.__socket_client.send("close".encode())
+            self.__socket_client.close()
+            self.thread_reception.terminate()
+            self.info.clear()
+            self.statut.setText("Déconnecté")
+            self.statut.setStyleSheet("background-color: red; font-weight: bold; color: black;")
+            QMessageBox.information(self, "Deconnexion", "Deconnexion réussie")
+            self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.statut.setText("Connecté")
+            self.statut.setStyleSheet("background-color: blue; font-weight: bold; color: black;")
+            QMessageBox.information(self, "Connecté", "Connexion réussie")
+        else:
+            QMessageBox.warning(self, "Deconnexion", "Déja déconnecté")
 
 
     def __actionAide(self):
         QMessageBox.information(self, "Aide","Choisissez une information du pc, de la vm que vous voulez récupérer (l'os, le nom, l'ip, ...).")
 
 
+
+####                            ####
+#### PARTIE THREAD DE RECEPTION ####
+####                            ####
+
+class ThreadReception(QThread):
+    def __init__(self, __socket_client, info):
+        super().__init__()
+        self.socket_client = __socket_client
+        self.info = info
+        self.exit_event = threading.Event()
+
+    def run(self):
+        flag = True
+        while flag == True:
+            if self.exit_event.is_set():
+                print ("exit")
+                flag = False
+            try:
+                data = self.socket_client.recv(1024)
+                self.info.append(f"Client> {data.decode('utf-8')}")
+                # Refresh the QPlainTextEdit
+                self.info.verticalScrollBar().setValue(self.serverreply.verticalScrollBar().maximum())
+                self.info.update()
+                # Create a log file for each client
+                # Write the data received from the client to the log file
+            except:
+                pass
+
+    def stop(self):
+        self.exit_event.set()
+        self.wait()
+
+
 ### MAIN ###
 
-if __name__ == '__main__':
-    
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     app.exec()
+
+
+if __name__ == '__main__':
+    main()
